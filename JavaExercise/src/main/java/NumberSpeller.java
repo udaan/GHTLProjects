128be96a351bc9cd9787afce3fde1ed1126c5d9e
udaan <nikam_s@yahoo.com>
2017-01-20 16:04:28 +0000

    initial test scenario coded

--- JavaExercise/src/main/java/NumberSpeller.java
+++ JavaExercise/src/main/java/NumberSpeller.java
@@ -2,8 +2,21 @@
  * Created by GHTL on 20/01/2017.
  */
 public class NumberSpeller {
+    private final String[] UnitWords = new String[]{"zero", "one", "two", "three", "four", "five",
+                                                    "six", "seven", "eight", "nine", "ten",
+                                                    "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+                                                    "sixteen", "seventeen", "eighteen", "ninteen"};
 
     public String spellOut(int number) {
-        return "";
+        if (number == 0) {
+            return UnitWords[0];
+        }
+
+        String words = "";
+        if (number < 20) {
+            words += UnitWords[number];
+        }
+
+        return words;
     }
 }

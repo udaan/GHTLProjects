684849da8d59568c768d33c33d253dd78408fd16
udaan <nikam_s@yahoo.com>
2017-01-20 17:36:46 +0000

    code fix

--- JavaExercise/src/main/java/Rack.java
+++ JavaExercise/src/main/java/Rack.java
@@ -5,22 +5,37 @@
  */
 public class Rack {
     private int[] balls;
-    private int[] originalSortedballs = new int[60];
+    private int[] originalSortedList = new int[60];
     private int currentSize;
 
     public int[] getBalls() {
-        if(balls == null) {
+        if (balls == null) {
             return new int[0];
         }
         return balls;
     }
 
     public void addBallAndSort(int number) {
-        if(currentSize == 0) {
-            originalSortedballs[0] = number;
-            //balls = originalSortedballs;
+        if (currentSize == 0) {
+            originalSortedList[0] = number;
             balls = new int[++currentSize];
             balls[0] = number;
+        } else {
+            int position = currentSize;
+            for (int i = currentSize - 1; i >= 0; i--) {
+                if (number < originalSortedList[i]) {
+                    originalSortedList[i + 1] = originalSortedList[i];
+                    position = i;
+                } else {
+                    break;
+                }
+            }
+
+            originalSortedList[position] = number;
+            currentSize++;
+            balls = new int[currentSize];
+
+            System.arraycopy(originalSortedList, 0, balls, 0, currentSize);
         }
     }
 }
